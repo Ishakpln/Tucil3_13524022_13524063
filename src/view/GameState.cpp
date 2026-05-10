b37c60fd1c5d4104321f2114546007eb8649e44b
LutherCeltors <lutherceltors@gmail.com>
2026-05-10 16:00:01 +0700

    feat: add choosen player state in gameState

--- src/view/GameState.cpp
+++ src/view/GameState.cpp
@@ -4,9 +4,9 @@ GameState::GameState():
     playMode(PlayMode::SENTINEL),
     boardSelected(false),
     selectedBoardPath("null"),
-    playerSelected(false),
-    playerImagePath("null"),
     board(),
+    playerSelected(false),
+    playerType("Baby"),
     gameOver(false),
     winning(false),
     resultGenerated(false),
@@ -16,9 +16,11 @@ GameState::GameState():
 
 PlayMode GameState::getPlayMode() const { return this->playMode; }
 Board GameState::getBoard() const {return this->board;}
+const Board& GameState::getBoardRef() const {return this->board;}
 bool GameState::isBoardSelected() const {return this->boardSelected;}
 const char* GameState::getStringPath() const {return this->selectedBoardPath.c_str();}
 bool GameState::isPlayerSelected() const {return this->playerSelected;}
+const std::string& GameState::getPlayerType() const {return this->playerType;}
 bool GameState::isGameOver() const {return this->gameOver;}
 bool GameState::isWinning() const {return this->winning;}
 bool GameState::isResultGenerated() const{return this->resultGenerated;}
@@ -26,9 +28,11 @@ bool GameState::isHasResult() const {return this->hasResult;}
 SolveResult GameState::getResult() const {return this->result;}
 const char* GameState::getErrorMessage() const {return this->errorMessage.c_str();}
 void GameState::setPlayMode(PlayMode p) { this->playMode = p; }
-void GameState::setBoard(Board& b) { this->board = b; }
-bool GameState::setBoardSelected(bool nVal) { this->boardSelected = nVal; }
+void GameState::setBoard(const Board& b) { this->board = b; }
+void GameState::setBoardSelected(bool nVal) { this->boardSelected = nVal; }
 void GameState::setStringPath(std::string newPath) { this->selectedBoardPath = newPath; }
+void GameState::setPlayerSelected(bool nVal) { this->playerSelected = nVal; }
+void GameState::setPlayerType(std::string newType) { this->playerType = newType.empty() ? "Baby" : newType; }
 void GameState::setGameOver(bool nVal) { this->gameOver = nVal; }
 void GameState::setWinning(bool nVal) { this->winning = nVal; }
 void GameState::setIsResultGenerated(bool nVal) { this->resultGenerated = nVal; }
@@ -36,14 +40,16 @@ void GameState::setIsHasResult(bool nVal) { this->hasResult = nVal; }
 void GameState::setResult(SolveResult newRes) { this->result = newRes; }
 void GameState::setErrorMessage(std::string errorMsg) { this->errorMessage = errorMsg; }
 void GameState::clearErrMsg() { this->errorMessage = "null"; }
-void GameState::clearBoard() { this->board = Board(); }
-void GameState::clearResult() { this->result = SolveResult(); }
+void GameState::clearBoard() { this->board = Board(); this->boardSelected = false; this->selectedBoardPath = "null"; }
+void GameState::clearResult() { this->result = SolveResult(); this->hasResult = false; this->resultGenerated = false; }
 void GameState::reset()
 {
     this->playMode = PlayMode::SENTINEL;
     this->boardSelected = false;
     this->selectedBoardPath = "null";
     this->board = Board();
+    this->playerSelected = false;
+    this->playerType = "Baby";
     this->gameOver = false;
     this->winning = false;
     this->resultGenerated = false;

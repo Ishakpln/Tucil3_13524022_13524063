c45a312ea04285be842ba0372dedd9c4b685a011
LutherCeltors <lutherceltors@gmail.com>
2026-05-10 16:04:14 +0700

    feat: implement BoardEditor scene

--- include/view/scenes/BoardEditor.hpp
+++ include/view/scenes/BoardEditor.hpp
@@ -6,6 +6,7 @@ class BoardEditor : public Scene
 {
 private:
     GameState& gameState;
+    SceneType requestedScene;
 
 public:
     BoardEditor(GameState& gs);
--- src/view/scenes/BoardEditor.cpp
+++ src/view/scenes/BoardEditor.cpp
@@ -1,7 +1,20 @@
 #include "view/scenes/BoardEditor.hpp"
+#include "library/raygui.h"
 
-BoardEditor::BoardEditor(GameState& gs): gameState(gs){}
+BoardEditor::BoardEditor(GameState& gs): gameState(gs), requestedScene(SceneType::BoardEditor){}
 
 void BoardEditor::update() { }
-void BoardEditor::draw() { }
-SceneType BoardEditor::nextScene() { }
+void BoardEditor::draw() {
+    DrawText("BOARD EDITOR", 240, 30, 36, BLACK);
+    DrawText("Editor placeholder: use existing text files in ./test for now.", 120, 150, 22, DARKGRAY);
+    DrawText("Logical chars: X obstacle, * path, L lava, Z start, O goal, 0-9 checkpoints", 80, 190, 20, DARKGRAY);
+
+    if (GuiButton(Rectangle{20, 20, 150, 45}, "Main Menu")) {
+        gameState.reset();
+        requestedScene = SceneType::MainMenu;
+    }
+    if (GuiButton(Rectangle{GetScreenWidth()/2.0f - 100.0f, GetScreenHeight() - 90.0f, 200.0f, 50.0f}, "Back to Boards")) {
+        requestedScene = SceneType::SelectBoard;
+    }
+}
+SceneType BoardEditor::nextScene() { return requestedScene; }
